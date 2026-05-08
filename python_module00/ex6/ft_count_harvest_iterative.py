366716b4304055942f67d69bf6f4efbc6866065c
Sorin ROSU <sorinrosu.pro@gmail.com>
2026-05-08 23:19:25 +0200

    use of range(), for loop & recursive methode to print each day until harvest

---
+++ python_module00/ex6/ft_count_harvest_iterative.py
@@ -0,0 +1,6 @@
+def ft_count_harvest_iterative():
+    days = input("Days until harvest: ")
+    x = range(1, int(days) + 1)
+    i = 1
+    for i in x:
+        print("Day: ", i)
---
+++ python_module00/ex6/ft_count_harvest_recursive.py
@@ -0,0 +1,9 @@
+def ft_count_helper(days, current):
+    if current > days:
+        return
+    print("Day: ", current)
+    ft_count_helper(days, current + 1)
+
+def ft_count_harvest_recursive():
+    days = input("Days until harvest: ")
+    ft_count_helper(int(days), 1)
